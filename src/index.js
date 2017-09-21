33640e1c45f9bf5bac7bdc9d76c2d13afbf75491
Jonathan Huang <grokut@gmail.com>
2017-09-20 22:23:50 -0700

    split pathname

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { connection } from "./services/connection";
 import { renderAll } from "./services/renderer";
 
-switch (window.location.pathname) {
-  case "/splom.html":
+const pathname = window.location.pathname.split("/")
+const route = pathname[pathname.length - 1]
+
+switch (route) {
+  case "splom.html":
     require("./charts/splom");
-  case "/lines.html":
+  case "lines.html":
     require("./charts/overview-detail-line");
     require("./charts/bar");
     require("./charts/faceted-line");
